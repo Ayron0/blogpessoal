6d8110a7d0a8c775f2b674fe4c36ed534b6376bf
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-10-06 20:38:47 -0300

    feat: adicionar o decorador @ApiProperty() nos atributos do usuario

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,31 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  process.env.TZ = '-03:00' //fuso horario brasileiro
+  const config = new DocumentBuilder()
+    .setTitle('Blog Pessoal')
+    .setDescription('Projeto Blog Pessoal')
+    .setContact(
+      'Generation Brasil',
+      'http://www.generationbrasil.online',
+      'generation@email.com',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+
+  process.env.TZ = '-03:00'; //fuso horario brasileiro
 
   app.useGlobalPipes(new ValidationPipe()); // Vai ser usado para fazer validações no projeto
-  
+
   app.enableCors();
-  
 
   await app.listen(process.env.PORT ?? 4000);
 }
-bootstrap();
+void bootstrap();
--- src/usuario/entities/usuario.entity.ts
+++ src/usuario/entities/usuario.entity.ts
@@ -1,31 +1,35 @@
-import { IsEmail, IsNotEmpty, MinLength } from "class-validator"
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
-import { Postagem } from "../../Postagem/entities/postagem.entity"
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Postagem } from '../../Postagem/entities/postagem.entity';
 
-@Entity({name: "tb_usuarios"})
+@Entity({ name: 'tb_usuarios' })
 export class Usuario {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn() 
-    id: number
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  @ApiProperty()
+  nome: string;
 
-    @IsNotEmpty()
-    @Column({length: 255, nullable: false}) 
-    nome: string
+  @IsEmail() // Verifica o padrão de email
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  @ApiProperty({ example: 'email@email.com.br' })
+  usuario: string;
 
-    @IsEmail() // Verifica o padrão de email
-    @IsNotEmpty()
-    @Column({length: 255, nullable: false })
-    usuario: string
+  @MinLength(8) // força a senha ter no Minimo 8 caracteres
+  @IsNotEmpty()
+  @Column({ length: 255, nullable: false })
+  @ApiProperty()
+  senha: string;
 
-    @MinLength(8) // força a senha ter no Minimo 8 caracteres
-    @IsNotEmpty()
-    @Column({length: 255, nullable: false }) 
-    senha: string
+  @Column({ length: 5000 }) //nullable que dizer não é obrigatorio ter foto
+  @ApiProperty()
+  foto: string;
 
-    @Column({length: 5000 }) //nullable que dizer não é obrigatorio ter foto
-    foto: string
-
-    @OneToMany(() => Postagem, (postagem) => postagem.usuario)
-    postagem: Postagem[]
-
+  @ApiProperty()
+  @OneToMany(() => Postagem, (postagem) => postagem.usuario)
+  postagem: Postagem[];
 }
