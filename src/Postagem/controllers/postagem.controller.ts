52b063480b33d14f297c772d8adc27a3a9817481
Ayron Paulo de Souza Sant' Anna <ayronpaulo2r@gmail.com>
2025-10-06 21:43:43 -0300

    feat: Swagger finalizado

--- src/Postagem/controllers/postagem.controller.ts
+++ src/Postagem/controllers/postagem.controller.ts
@@ -1,49 +1,64 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, UseGuards } from "@nestjs/common";
-import { Postagem } from "../entities/postagem.entity";
-import { PostagemService } from "../services/postagem.services";
-import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
+import { Postagem } from '../entities/postagem.entity';
+import { PostagemService } from '../services/postagem.services';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+@ApiTags('Postagem')
 @UseGuards(JwtAuthGuard)
-@Controller("/postagens")
+@Controller('/postagens')
+@ApiBearerAuth()
 export class PostagemController {
-    constructor(private readonly postagemService: PostagemService) { }
-    //a variavel postagemService esta representando a class PostagemService.Ele faz uma requisição para PostagemService
+  constructor(private readonly postagemService: PostagemService) {}
+  //a variavel postagemService esta representando a class PostagemService.Ele faz uma requisição para PostagemService
 
-    @Get()
-    @HttpCode(HttpStatus.OK)// Esse status vai ser mostrado no Isomnia. Pode ser alterado para mostrar outra coisa
-    findAll(): Promise<Postagem[]> { // Se coloca o mesmo nome do metodo que estar em postagem.service 
-        return this.postagemService.findAll()
-    }
+  @Get()
+  @HttpCode(HttpStatus.OK) // Esse status vai ser mostrado no Isomnia. Pode ser alterado para mostrar outra coisa
+  findAll(): Promise<Postagem[]> {
+    // Se coloca o mesmo nome do metodo que estar em postagem.service
+    return this.postagemService.findAll();
+  }
 
-    @Get("/:id")
-    @HttpCode(HttpStatus.OK)
-    findByid(@Param("id", ParseIntPipe) id: number): Promise<Postagem> {
-        return this.postagemService.findByid(id);
-    }
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findByid(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
+    return this.postagemService.findByid(id);
+  }
 
-    @Get("/titulo/:titulo")
-    @HttpCode(HttpStatus.OK)
-    findByAllTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
-        return this.postagemService.findAllByTitulo(titulo);
-    }
+  @Get('/titulo/:titulo')
+  @HttpCode(HttpStatus.OK)
+  findByAllTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+    return this.postagemService.findAllByTitulo(titulo);
+  }
 
-    @Post()
-    @HttpCode(HttpStatus.CREATED)
-    create(@Body() postagem: Postagem): Promise<Postagem> {
-        return this.postagemService.create(postagem);
-    }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.create(postagem);
+  }
 
-    @Put()
-    @HttpCode(HttpStatus.OK)
-    update(@Body() postagem: Postagem): Promise<Postagem> {
-        return this.postagemService.update(postagem);
-    }
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() postagem: Postagem): Promise<Postagem> {
+    return this.postagemService.update(postagem);
+  }
 
-    @Delete("/:id")
-    @HttpCode(HttpStatus.NO_CONTENT)
-    delete(@Param('id', ParseIntPipe) id: number) {
-        return this.postagemService.delete(id);
-    }
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.postagemService.delete(id);
+  }
 }
 
 // Postagem[] lista
--- src/Postagem/entities/postagem.entity.ts
+++ src/Postagem/entities/postagem.entity.ts
@@ -10,28 +10,36 @@ import {
 } from 'typeorm';
 import { Tema } from '../../tema/entities/tema.entity';
 import { Usuario } from '../../usuario/entities/usuario.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'tb_postagens' })
 export class Postagem {
+
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number; //id PRIMARY KEY AUTO_INCREMENT
 
+  @ApiProperty()
   @IsNotEmpty()
   @Column({ length: 100, nullable: false })
   titulo: string; //titulo VARCHAR (100) NOT NULL
 
+  @ApiProperty()
   @IsNotEmpty()
   @Column({ length: 1000, nullable: false })
   texto: string; //titulo VARCHAR (1000) NOT NULL
 
+  @ApiProperty()
   @UpdateDateColumn()
   data: Date; // data DATE(6) UPDATE_TIMESTAMP
 
+  @ApiProperty({ type: () => Tema })
   @ManyToOne(() => Tema, (tema) => tema.postagem, {
     onDelete: 'CASCADE',
   })
   tema: Tema;
 
+  @ApiProperty({ type: () => Usuario })
   @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
     onDelete: 'CASCADE',
   })
